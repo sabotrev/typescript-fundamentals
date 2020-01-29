952caae09cb530575ba67c4ad518f7133b3ed343
TSabo <sabotrev@gmail.com>
2020-01-28 17:43:39 -0800

    chore: added map & reduce exercise

--- challenges/dict/src/index.ts
+++ challenges/dict/src/index.ts
@@ -1,7 +1,42 @@
-export type Dict = {};
+export type Dict<T> = {
+  [k: string]: T | undefined;
+};
 
 // Array.prototype.map, but for Dict
-export function mapDict() {}
+export function mapDict<T, S>(
+  dict: Dict<T>,
+  fn: (arg: T, idx: number) => S
+): Dict<S> {
+  const out: Dict<S> = {};
+  Object.keys(dict).forEach((dKey, idx) => {
+    const thisItem = dict[dKey];
+    if (typeof thisItem !== "undefined") {
+      out[dKey] = fn(thisItem, idx);
+    }
+  });
+
+  return out;
+}
+
+console.log(mapDict({ a: "a", b: "b", c: "c" }, str => ({ val: str })));
 
 // Array.prototype.reduce, but for Dict
-export function reduceDict() {}
+export function reduceDict<T, S>(
+  dict: Dict<T>,
+  reducer: (acc: S, curr: T, idx: number) => S,
+  initialValue: S
+) {
+  let acc: S = initialValue;
+
+  Object.keys(dict).forEach((dKey, idx) => {
+    const thisItem = dict[dKey];
+
+    if (typeof thisItem !== "undefined") {
+      acc = reducer(acc, thisItem, idx);
+    }
+  });
+
+  return acc;
+}
+
+console.log(reduceDict({ a: 1, b: 2, c: 3 }, (acc, val) => acc + val, 0));
